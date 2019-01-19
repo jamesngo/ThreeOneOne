f395b33610a3ab64bc123c9f8bb97b114fa7f8ef
mjpon <mjpon@ucsc.edu>
2019-01-19 01:52:23 -0800

    another src folder

--- threeoneonescc/src/components/header.js
+++ threeoneonescc/src/components/header.js
@@ -1,17 +1,7 @@
-// /** @format */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-// //
-
 // Import a library to help create a component
 import * as React from 'react';
 // import ReactNative from 'react-native';
-import {Text, Header, AppRegistry } from 'react-native';
-// import Header from './src/components/header'
+import { Text , AppRegistry } from 'react-native';
 // if you were to do it wholistically
 // import * as ReactNative from 'react-native';
 
@@ -25,13 +15,9 @@ import {Text, Header, AppRegistry } from 'react-native';
          // if you were to do it wholistically
         //  <ReactNative.Text>Some Text</ReactNative.Text>
         //  <ReactNative.Text>Some Text</ReactNative.Text>
-//         <Header
-//   placement="left"
-//   leftComponent={{ icon: 'menu', color: '#fff' }}
-//   centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-//   rightComponent={{ icon: 'home', color: '#fff' }}
-// />
-        <Text>Some Text</Text>
+             <Text>Some Text</Text>
+
+
      );
 
  };
@@ -41,5 +27,4 @@ import {Text, Header, AppRegistry } from 'react-native';
 // Render it to the device.
 // if you were to do it wholistically
 // ReactNative.AppRegistry.registerComponent('albums2',() => App);
-AppRegistry.registerComponent('threeoneonescc',() => App);
-
+export default Header;
