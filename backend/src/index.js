79e61153a40134be376284b84ad1815e17f9d755
Nikhil Sheth <nps225@gmail.com>
2019-01-19 15:14:26 -0800

    updated the index.js
    now supports for http requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,20 @@
 //Depenedencies
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-//const cassandra = require('cassandra-driver');
-
-//Set up Cassandra Server Connection
-//const authProvider = new cassandra.auth.PlainTextAuthProvider('cassandra', 'cassandra');
-//Set the auth provider in the clientOptions when creating the Client instance
-//const client = new cassandra.Client({contactPoints:['127.0.0.1'], authProvider: authProvider });
-
-/*client.connect(function (err) {
-  console.log(err);
-});*/
 const cassandra = require('cassandra-driver');
 const authProvider = new cassandra.auth.PlainTextAuthProvider('backend', 'password');
 
-const client = new cassandra.Client({ contactPoints: ['169.233.239.56'],localDataCenter: 'datacenter1', authProvider: authProvider});
+
+
+const client = new cassandra.Client({ contactPoints: ['169.233.207.180'],localDataCenter: 'datacenter1', authProvider: authProvider});
+
+app.listen(8080,'169.233.207.180');
 client.connect(function (err) {
-  console.log(err);
+  if(err){
+    console.log(err);
+  }
+  app.get("/", (req, res) => res.send("Hello World!"));
+
+
 });
