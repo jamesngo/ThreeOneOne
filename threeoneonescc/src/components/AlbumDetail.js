b8592446c1a639db9ddb63d93d3192c8313b8a5c
mjpon <mjpon@ucsc.edu>
2019-01-19 18:36:48 -0800

    getting the card view almost done

--- threeoneonescc/src/components/AlbumDetail.js
+++ threeoneonescc/src/components/AlbumDetail.js
@@ -1,22 +1,34 @@
 import React from 'react';
-import{ Text } from 'react-native';
+import{ Text , View, Image} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    const{title, artist, thumbnail_image} = album;
        return(
 
             <Card>
                 <CardSection>
-                <Text>{props.album.title}</Text>
-                </CardSection>
-
-                <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image source={{uri: thumbnail_image}}/>
+                </View>
+                <View>
+                <Text>{title}</Text>
+                <Text>{artist}</Text>
+                </View>
                 </CardSection>
              </Card>
             );
 
 };
+
+const styles = {
+    headerContentStyle:{
+        flexDirection: 'column',
+        justifyContent:'space-around'
+    }
+};
+
+
 
 export default AlbumDetail;
