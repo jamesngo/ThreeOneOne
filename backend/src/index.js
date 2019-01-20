faa10f196e3d7fe84afd480da85c921f80be1a04
James Ngo <jnngo@ucsc.edu>
2019-01-19 20:38:28 -0800

    added routes for user creation

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,12 +10,13 @@ const cassandra = require('cassandra-driver');
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const authProvider = new cassandra.auth.PlainTextAuthProvider('backend', 'password');
+const ip = require("ip");
+const a = ip.address();
 
 
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1', '169.233.239.56'],localDataCenter: 'datacenter1', authProvider: authProvider});
 
-const client = new cassandra.Client({ contactPoints: ['localhost'],localDataCenter: 'datacenter1', authProvider: authProvider});
-
-app.listen(8080,'169.233.207.180');
+app.listen(8080,a);
 app.use(jsonParser);
 
 client.connect(function (err) {
@@ -23,6 +24,7 @@ client.connect(function (err) {
     console.log(err);
   }
   app.post("/submitTicket", (req, res) => routes.recieveTicket(client,req,res));
-
-
+	app.get("/getTickets/:userId", (req, res) => routes.getTickets(client, req, res));
+	app.post("/createUser", (req, res) => routes.createUser(client, req, res));
+	console.log("opened routes");
 });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,51 @@
 // routes.js
 // =========
-const uuidv1 = require('uuid/v1');
-const ip = require("ip");
-const a = ip.address();
+const uuid = require('cassandra-driver').types.Uuid;
 
 module.exports.recieveTicket = function(client,req,res) {
   //recieve a .json file
   var ticket = req.body;
-  ticket["id"] = uuidv1() + a ; // ⇨ '3b99e3e0-7598-11e8-90be-95472fb3ecbd'
+	ticket["date"] = + Date();
+	ticket["id"] = uuid.random();
   //console.log('INSERT INTO threeoneone.test JSON \'' + JSON.stringify(ticket) + '\' IF NOT EXISTS;');
   //create row in table
-  client.execute('INSERT INTO threeoneone.test JSON \'' + JSON.stringify(ticket) + '\' IF NOT EXISTS;', function (err, result) {
+  client.execute('INSERT INTO threeoneone.tickets JSON \'' + JSON.stringify(ticket) + '\';', function (err, result) {
     if(err){
       console.log(err);
     }
   });
+	
+	client.execute('UPDATE threeoneone.users SET tickets = tickets + { '+ ticket["id"] + '} where id = '+ ticket["userid"] + ';', function(err, result) {
+		if(err) {
+			console.log(err);
+		}
+	});
 
   res.sendStatus(200);
 }
 
 //https://stackoverflow.com/questions/19696240/proper-way-to-return-json-using-node-or-express
-module.export.sendTicket = function(client,req,res){
+module.exports.getTickets = function(client,req,res){
+	var userId= req.params.userId;
+	
+	client.execute('SELECT tickets FROM threeoneone.users WHERE id = '+uuid.fromString(userId)+';', function(err, result) {
+		if(err) {
+			console.log(err);
+		}
+		console.log(result);
+  	res.sendStatus(200);
+	});
 
+}
+
+module.exports.createUser = function(client, req, res) {
+	var user = req.body;
+	user["id"] = uuid.random();
+	user["datejoined"] = new Date().getTime();	
+	client.execute('INSERT INTO threeoneone.users JSON\'' + JSON.stringify(user) + '\';', function (err, result) {
+		if(err) {
+			console.log(err);
+		}
+	});
+	res.sendStatus(200);
 }
